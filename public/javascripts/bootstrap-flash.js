6990f1837e343c29aad05499fb7ec2a001b61a91
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-05-16 16:38:44 -0600

    creating home page skeleton

---
+++ public/javascripts/bootstrap-flash.js
@@ -0,0 +1,3 @@
+console.log("bootstrap flash")
+
+$().alert('close') // closes data-dismiss="alert" http://getbootstrap.com/javascript/#alerts-methods
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    title: 'React Robots!',
+    page_title: 'Robots',
+    robots:[
+      {name:"c3po", description:"specializes in language translation"},
+      {name:"r2d2", description:"holds a secret message"},
+      {name:"bb8",  description:"rolls around"}
+    ]
+  });
 });
 
 module.exports = router;
