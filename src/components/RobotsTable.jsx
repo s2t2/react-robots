fc625cf73c41884640e7d675c80c98fa58d3953c
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-05-16 17:56:36 -0600

    creating table rows dynamically

--- src/components/RobotsTable.jsx
+++ src/components/RobotsTable.jsx
@@ -4,14 +4,12 @@ import ReactDOM from 'react-dom';
 var RobotsTable = React.createClass({
   render: function(){
     var robots = [
-      {name:"c3po", description:"specializes in language translation"},
-      {name:"r2d2", description:"holds a secret message"},
-      {name:"bb8",  description:"rolls around"}
+      {id: 1, name:"c3po", description:"specializes in language translation"},
+      {id: 2, name:"r2d2", description:"holds a secret message"},
+      {id: 3, name:"bb8",  description:"rolls around"}
     ];
-    var robot = robots[0];
 
     return (
-
       <table className="table table-bordered table-hover table-responsive" style={{width:"100%"}}>
         <thead>
           <tr>
@@ -20,13 +18,18 @@ var RobotsTable = React.createClass({
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td><a href={'robots/'+robot.name} >{robot.name}</a></td>
-            <td>{robot.description}</td>
-          </tr>
+          {
+            robots.map( function(robot){
+              return (
+                <tr key={robot.id}>
+                  <td><a href={'robots/'+robot.name} >{robot.name}</a></td>
+                  <td>{robot.description}</td>
+                </tr>
+              );
+            })
+          }
         </tbody>
       </table>
-
     )
   }
 });
