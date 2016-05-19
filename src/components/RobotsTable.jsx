995f265c72ca584861788670e251d62a3117226c
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-05-18 20:13:28 -0600

    translating more of the page into react components; passing title text as props

--- src/components/RobotsTable.jsx
+++ src/components/RobotsTable.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 var moment = require('moment-timezone');
 
 var RobotsTable = React.createClass({
--- src/entry.js
+++ src/entry.js
@@ -5,10 +5,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './components/Header.jsx'
+import PageTitle from './components/PageTitle.jsx';
 import RobotsTable from './components/RobotsTable.jsx';
 import Footer from './components/Footer.jsx';
 
-ReactDOM.render(<Header/>, document.getElementById('header') );
+ReactDOM.render(<Header site_title={"React Robots!"} />, document.getElementById('header') );
+ReactDOM.render(<PageTitle page_title={"Robots"} />, document.getElementById('page-title') );
 ReactDOM.render(<RobotsTable/>, document.getElementById('robots-table') );
 ReactDOM.render(<Footer/>, document.getElementById('footer') );
 
