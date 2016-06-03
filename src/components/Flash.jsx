3654e2f8a7fd2b738613620c960554ff1540e1c5
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-03 17:23:42 -0600

    rendering flash messages from state

--- src/components/Flash.jsx
+++ src/components/Flash.jsx
@@ -1,50 +1,49 @@
 import React from 'react';
 
 var Flash = React.createClass({
-  render: function(){
-    var component = this; // temp workaround
-    console.log(component.flash)
-
-    return (
-      <div>
-        <p>my flash</p>
-        <p>{this.state.flash.test[0]}</p>
-        {
-          ["success","info","warning","danger","test"].map(function(alertClassName){
-            return (
-              <div key={alertClassName} className="alert alert-success alert-dismissible" role="alert">
-                <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                {component.state.flash["test"][0]}
-              </div>
-            );
-          })
-        }
-      </div>
-    )
-  },
-
   getInitialState: function(){
     return (
       {
         flash: {
-          test: ["this is a flash message"]
+          warning: ["this is a flash message"],
+          danger: ["dangerous", "duo"]
         }
       }
     )
+  },
+
+  render: function(){
+    var component = this;
+    console.log(component.state.flash)
+
+    var flashMessages = Object.keys(component.state.flash).map(function(messageType){
+      var messages = component.state.flash[messageType];
+      console.log("MESSAGES", messageType, messages);
+
+      return (
+        messages.map(function(messageContent){
+          var messageId = messages.indexOf(messageContent);
+          console.log("MESSAGE #"+messageId , messageType, messageContent);
+
+          return (
+            <div key={messageType + "-" + messageId} className={"alert alert-" + messageType + " alert-dismissible"} role="alert">
+              <button type="button" className="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              {messageContent}
+            </div>
+          );
+        })
+      )
+    });
+
+    console.log(flashMessages)
+
+    return (
+      <div>
+        <p>my flash</p>
+        {flashMessages}
+      </div>
+    )
   }
-
-  //bootstrapMessages: function(flash){
-  //  if(messages){
-  //    Object.keys(messages).forEach(function (type) {
-  //      messages[type].forEach(function (message) {
-  //      <div class="alert alert-<%= type %> alert-dismissible" role="alert">
-  //        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-//        <%= message %>
-  //      </div>
-//    })
-  //  })
-  //}
-
 });
 
 module.exports = Flash;
