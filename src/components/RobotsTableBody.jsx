c22afe968e84613ba070388bfcb03895e1c8d49b
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-07 16:00:41 -0600

    starting to decompose this complex method

--- src/components/RobotsTableBody.jsx
+++ src/components/RobotsTableBody.jsx
@@ -37,12 +37,12 @@ var RobotsTableBody = withRouter (
 
     componentDidMount: function(){
       console.log("TABLE BODY DID MOUNT");
-      this.getRobots(this.props.params);
+      this.determineRobots(this.props.params);
     },
 
     componentWillReceiveProps: function(nextProps) {
       console.log("TABLE BODY WILL RECEIVE PROPS");
-      this.getRobots(nextProps.params)
+      this.determineRobots(nextProps.params)
     },
 
     componentWillUpdate: function(nextProps, nextState) {
@@ -53,52 +53,62 @@ var RobotsTableBody = withRouter (
     // MY FUNCTIONS
     //
 
-    getRobots: function(paramz){
+    determineRobots: function(paramz){
+      console.log("DETERMINING ROBOTS BASED ON PARAMS", paramz);
+      var robots;
       if(paramz.id){
-        console.log("GET ROBOT");
-        $.ajax({
-          url: '/api/robots/'+paramz.id,
-          dataType: 'json',
-          cache: false,
-          success: function(data) {
-            console.log("SET ROBOT");
-            this.setState({robots: [data]});
-          }.bind(this),
-          error: function(xhr, status, err) {
-            console.error(xhr, status, err);
-            console.log("COULDN'T SET ROBOT");
-            this.props.router.push({
-              pathname: '/',
-              state: {
-                robots: [],
-                flash: {danger: ["Couldn't find robot #"+paramz.id]}
-              }
-            });
-          }.bind(this)
-        });
+        this.setRobot(paramz.id);
       } else {
-        console.log("GET ROBOTS");
-        $.ajax({
-          url: '/api/robots',
-          dataType: 'json',
-          cache: false,
-          success: function(data) {
-            console.log("GOT ROBOTS")
-            this.setState({robots: data});
-          }.bind(this),
-          error: function(xhr, status, err) {
-            console.error(xhr, status, err);
-            console.log("GOT NO ROBOTS");
-            this.props.router.push({
-              pathname: '/',
-              state: {
-                robots: [],
-                flash: {danger: ["Couldn't find any robots"]}
-              }
-            });
-          }.bind(this)
-        });
+        this.setRobots();
       };
+    },
+
+    setRobot: function(robotId){
+      var requestUrl = '/api/robots/'+robotId;
+      console.log("AJAX REQUEST", requestUrl)
+      $.ajax({
+        url: requestUrl,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+          console.log("REQUEST SUCCESS", data);
+          this.setState({robots: [data]});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.log("REQUEST ERROR", xhr, status, err);
+          this.props.router.push({
+            pathname: '/',
+            state: {
+              robots: [],
+              flash: {danger: ["Couldn't find robot #"+robotId]}
+            }
+          });
+        }.bind(this)
+      });
+    },
+
+    setRobots: function(){
+      var requestUrl = '/api/robots';
+      console.log("AJAX REQUEST", requestUrl)
+      $.ajax({
+        url: requestUrl,
+        dataType: 'json',
+        cache: false,
+        success: function(data) {
+          console.log("REQUEST SUCCESS", data);
+          this.setState({robots: data});
+        }.bind(this),
+        error: function(xhr, status, err) {
+          console.log("REQUEST ERROR", xhr, status, err);
+          this.props.router.push({
+            pathname: '/',
+            state: {
+              robots: [],
+              flash: {danger: ["Couldn't find any robots"]}
+            }
+          });
+        }.bind(this)
+      });
     }
   })
 );
