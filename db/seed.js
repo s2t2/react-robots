ac232a1a9745633db01787b41786fbbacd2952c4
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-07 23:32:27 -0600

    refactoring

--- db/seed.js
+++ db/seed.js
@@ -1,20 +1,6 @@
 var db = require("../db"); // starts a mongoose connection
 var Robot = require("../models/robot");
 
-var robots = [
-    {name:"c3po", description:"specializes in language translation"},
-    {name:"r2d2", description:"holds a secret message"},
-    {name:"c3po", description:"specializes in language translation"},
-    {name:"r2d2", description:"holds a secret message"},
-    {name:"c3po", description:"specializes in language translation"},
-    {name:"r2d2", description:"holds a secret message"},
-    {name:"c3po", description:"specializes in language translation"},
-    {name:"r2d2", description:"holds a secret message"},
-    {name:"c3po", description:"specializes in language translation"},
-    {name:"r2d2", description:"holds a secret message"},
-    {name:"bb8",  description:"rolls around"}
-];
-
 Robot.find(function (err, bots) {
     if (err) return console.error(err);
     console.log("FOUND", bots.length, "ROBOTS TO BE DELETED")
@@ -23,7 +9,7 @@ Robot.find(function (err, bots) {
         if (err) return console.error(err);
         console.log("DELETED")
 
-        Robot.create(robots, function (err, new_bots) {
+        Robot.create(Robot.devRobots, function (err, new_bots) {
             console.log(new_bots)
             db.disconnect(); // close the connection, else it will keep running, which is appropriate for when the web server runs, but not for a script like this.
         });
