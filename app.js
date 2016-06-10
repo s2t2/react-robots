cb4bd4fab01ee9969506f8eda53f043f67c8fd31
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-09 22:10:11 -0600

    hoping to resolve an error whereby the bundle file exists on the server but the browser thinks its contents are that of the index.html file

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use('/', routes);
 app.use('/', api_routes);
