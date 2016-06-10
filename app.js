2c7ff11669a89461e43513e3645e312c72cb9de2
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-09 22:30:21 -0600

    the browser was recognizing the bundle in the root directory instead of the dist/ directory

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'dist')));
+//app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/dist', express.static(path.join(__dirname, 'dist'))); // recognize static files compiled/transpiled from react components
 
 app.use('/', routes);
 app.use('/', api_routes);
