a7513a7c24010758d642c4c52327abedcd45e1c5
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-10 21:04:13 -0600

    diagnosing test failure: need to defer test execution until after before block is done

--- db/recycle_robots.js
+++ db/recycle_robots.js
@@ -2,21 +2,22 @@ var db = require("../db"); // starts a mongoose connection
 var Robot = require("../models/robot");
 
 module.exports = function recycleRobots(){
-  Robot.find(function (err, bots) {
-    if (err){ return console.error(err); };
-    console.log("FOUND", bots.length, "RECORDS");
-
-    Robot.remove(bots, function (rmErr) {
-      if (rmErr){ return console.error(rmErr); };
-      console.log("DELETED", bots.length, "RECORDS");
-
-      Robot.create(Robot.defaultRobots(), function (crErr, newBots) {
-        if (crErr) { return console.error(crErr); };
-        console.log("CREATED", newBots.length, "RECORDS");
-        db.disconnect(); // close the connection, else it will keep running, which is appropriate for when the web server runs, but not for a script like this.
-      }); // Robot.create
-    }); // Robot.remove
-  }); // Robot.find
+  //Robot.find(function (err, bots) {
+  //  if (err){ return console.error(err); };
+  //  console.log("FOUND", bots.length, "RECORDS");
+//
+  //  Robot.remove(bots, function (rmErr) {
+  //    if (rmErr){ return console.error(rmErr); };
+  //    console.log("DELETED", bots.length, "RECORDS");
+//
+  //    Robot.create(Robot.defaultRobots(), function (crErr, newBots) {
+  //      if (crErr) { return console.error(crErr); };
+  //      console.log("CREATED", newBots.length, "RECORDS");
+  //      db.disconnect(); // close the connection, else it will keep running, which is appropriate for when the web server runs, but not for a script like this.
+  //    }); // Robot.create
+  //  }); // Robot.remove
+  //}); // Robot.find
+  console.log("RECYCLING HERE!")
 };
 
 //module.exports.recycleRobots = function(){
--- test/api_tests.js
+++ test/api_tests.js
@@ -2,40 +2,25 @@ var expect = require('expect');
 var request = require('supertest');
 
 var app = require('../app.js');
-//var recycleRobots = require('../db/recycle_robots');
-//var Robot = require('../models/robot');
+var recycleRobots = require('../db/recycle_robots');
+var Robot = require('../models/robot');
 
-//var robots = Robot.defaultRobots();
-var robots = [
-  {name:"c3po", description:"specializes in language translation"},
-  {name:"r2d2", description:"holds a secret message"},
-  {name:"c3po", description:"specializes in language translation"},
-  {name:"r2d2", description:"holds a secret message"},
-  {name:"c3po", description:"specializes in language translation"},
-  {name:"r2d2", description:"holds a secret message"},
-  {name:"c3po", description:"specializes in language translation"},
-  {name:"r2d2", description:"holds a secret message"},
-  {name:"c3po", description:"specializes in language translation"},
-  {name:"r2d2", description:"holds a secret message"},
-  {name:"bb8",  description:"rolls around"}
-]
+var defaultRobots = Robot.defaultRobots();
 
 function responseIsArrayOfRobots(res){
-  //console.log("BODY", res.text, res.body); // why is the body blank here but not in the assertions below? // jk this is logging now...
+  //console.log("BODY", res.text, res.body);
   expect(res.body).toBeA("object");
-  expect(res.body.length).toEqual(robots.length);
-  var matchingRobot = res.body.find(function(r){ return r.name == robots[0].name && r.description == robots[0].description });
+  expect(res.body.length).toEqual(defaultRobots.length);
+  var matchingRobot = res.body.find(function(r){ return r.name == defaultRobots[0].name && r.description == defaultRobots[0].description });
   console.log(matchingRobot);
   expect(matchingRobot).toExist();
 };
 
-//throw new Error("intentional err")
-
 describe("API", function(){
   describe("INDEX ROUTE", function(){
-    //before(function(){
-    //  recycleRobots();
-    //});
+    before(function(){
+      recycleRobots(); // need to wait until this is finished before running tests or else the tests will test sometime when the records are not there and throw an error due to an empty response
+    });
 
     it("should return an array of json objects", function(done){
       request(app).get("/api/robots")
@@ -43,10 +28,6 @@ describe("API", function(){
         .expect(200)
         .expect(responseIsArrayOfRobots)
         .end(done)
-        //.end(function(err, res){
-        //  console.log(res.body); // got the right body here
-        //  done()
-        //})
     });
   });
 });
