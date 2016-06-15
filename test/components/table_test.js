c3c2a2b319cc3528052d27bdc3d19037e632daa5
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-15 17:24:07 -0600

    completing detailed table component tests

--- test/components/table_test.js
+++ test/components/table_test.js
@@ -7,28 +7,46 @@ var defaultRobots = require('../../db/default_robots').defaultRobots();
 describe("Table", function(){
   before(function(done){  recycleRobots().then(done());  });
 
+  //
+  // INDEX PAGE
+  //
+
   context("when visited on the 'index' page", function(){
     before(function(){  return browser.visit('/');  });
 
     it("page should contain a heading", function(){
-      expect(browser.query("h2").innerHTML).toEqual("Robots")
-    })
+      expect(browser.query("h2").innerHTML).toEqual("Robots");
+    });
 
     it("page should contain a table", function(){
       browser.assert.element('table');
-    })
+    });
 
     it("table should contain a row per record", function(){
-      var rows = browser.queryAll("tbody tr")
+      var rows = browser.queryAll("tbody tr");
       expect(rows.length).toEqual(defaultRobots.length)
-    })
+    });
 
     describe("Row", function(){
       it("should contain a 'show page' link", function(){
         var robotId = browser.query("tbody tr td").innerHTML;
         var showPageLink = browser.query("tbody tr td a");
-        expect(showPageLink.href).toInclude("/robots/"+robotId)
-      })
-    })
-  })
-})
+        expect(showPageLink.href).toInclude("/robots/"+robotId);
+      });
+    });
+  });
+
+  //
+  // SHOW PAGE
+  //
+
+  context("when visited on the 'show' page", function(){
+    before(function(){  return browser.visit('/');  });
+    before(function(){  return browser.clickLink('bb8');  });
+
+    it("table should contain only one row", function(){
+      var rows = browser.queryAll("tbody tr");
+      expect(rows.length).toEqual(1)
+    });
+  });
+});
