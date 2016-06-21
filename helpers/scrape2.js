00bfeb6038fa7f1cbe5e59d2637090e45dc0ea20
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-21 01:14:01 -0400

    running browser automation tasks outside of test suite to gain familiarity

---
+++ helpers/scrape2.js
@@ -0,0 +1,73 @@
+var webdriver = require('selenium-webdriver'),
+    By = require('selenium-webdriver').By,
+    until = require('selenium-webdriver').until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('firefox')
+    .build();
+
+
+
+// INDEX
+
+driver.get('http://localhost:3000/')
+
+driver.getTitle().then(function(result){
+  console.log("SITE TITLE", result)
+})
+
+driver.findElement(By.tagName("h2")).then(function(element){
+  element.getText().then(function(text){
+    console.log("PAGE HEADER", text)
+  });
+})
+
+
+
+// NEW
+
+driver.findElement(By.partialLinkText('new')).click()
+
+
+driver.getTitle().then(function(result){
+  console.log("SITE TITLE", result)
+})
+
+driver.findElement(By.tagName("h2")).then(function(element){
+  element.getText().then(function(text){
+    console.log("PAGE HEADER", text)
+  });
+})
+
+
+// SUBMIT
+
+driver.findElement(By.xpath('//button[@type="submit"]')).click()
+
+driver.getTitle().then(function(result){
+  console.log("SITE TITLE", result)
+})
+
+driver.findElement(By.tagName("h2")).then(function(element){
+  element.getText().then(function(text){
+    console.log("PAGE HEADER", text)
+  });
+})
+
+
+// FLASH MESSAGES
+
+driver.findElements(By.css("div .alert")).then(function(elements){
+  console.log("MESSAGES", elements.length)
+
+  elements.forEach(function(element){
+    element.getText().then(function(text){
+      console.log("MESSAGE", text)
+    });
+  })
+
+})
+
+//driver.wait(until.titleIs('webdriver - Google Search'), 1500);
+
+driver.quit();
