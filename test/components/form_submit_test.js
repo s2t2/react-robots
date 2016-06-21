36d1313d8fac9ae17f6ee402e03f60243aba6dd3
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-21 02:09:07 -0400

    de-composing test by moving tasks into their proper mocha hooks

--- test/components/form_submit_test.js
+++ test/components/form_submit_test.js
@@ -3,8 +3,36 @@ var expect = require('expect');
 var driver = require("../../helpers/test_web_driver.js").driver,
     By = require("../../helpers/test_web_driver.js").By;
 
-describe("Form Submit", function(){
+// HELPER FUNCTIONS
 
+function getIndex(){
+  return driver.get('http://localhost:3000/');
+}
+
+function clickNew(){
+  return driver.findElement(By.partialLinkText('new')).click();
+}
+
+function clickSubmit(){
+  return driver.findElement(By.xpath('//button[@type="submit"]')).click();
+}
+
+function findMessages(){
+  return driver.findElements(By.css("div .alert"));
+}
+
+function logMessages(elements){
+  elements.forEach(function(element){
+    element.getText().then(function(text){
+      console.log("MESSAGE", text)
+    })
+  })
+}
+
+
+
+
+describe("Form Submit", function(){
   this.timeout(15000)
 
   //
@@ -12,34 +40,17 @@ describe("Form Submit", function(){
   //
 
   context("when visited on the 'new' page", function(){
-    //before(function(){
-    //  return driver.get('http://localhost:3000/');
-    //});
-    //before(function(){
-    //  return driver.findElement(By.partialLinkText('new')).click();
-    //});
-    //after(function(){
-    //  driver.quit();
-    //})
+    before(function(){  return getIndex().then(clickNew);  });
+    after(function(){  driver.quit(); })
 
     context("when submitted with invalid values", function(){
+      before(function(){  return clickSubmit();  })
+
       it("flash should include error messages", function(){
-
-        return driver.get('http://localhost:3000/').then(function(){
-          driver.findElement(By.partialLinkText('new')).click().then(function(){
-            driver.findElement(By.xpath('//button[@type="submit"]')).click().then(function(){
-              driver.findElements(By.css("div .alert")).then(function(elements){
-                console.log("MESSAGES", elements.length)
-
-                expect(elements.length).toEqual(5)
-
-                driver.quit();
-
-              }) // FIND MESSAGES
-            }) // CLICK SUBMIT
-          }) // CLICK NEW
-        }) // GET INDEX
-
+        return findMessages().then(function(elements){
+          console.log("MESSAGES", elements.length)
+          expect(elements.length).toEqual(5)
+        })
       })
     })
 
@@ -48,23 +59,6 @@ describe("Form Submit", function(){
 
 
 
-
-
-
-    //context("when submitted with an invalid value", function(){
-    //  before(function() {
-    //    return browser.fill("robotName", "Baker Bot").pressButton('Submit');
-    //  });
-//
-    //  it("flash should include an error message", function(){
-    //    expect(browser.queryAll(".alert-warning").length).toEqual(1)
-    //  })
-//
-    //  it("form values should be passed back", function(){
-    //    browser.assert.input("form input[name=robotName]", "Baker Bot");
-    //    browser.assert.input("form textarea[name=robotDescription]", "");
-    //  })
-    //})
 
     //context("when submitted with valid values", function(){
     //  before(function(done) {
