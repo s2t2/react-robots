194cc9599db3aa3459262028eade3d79a2c8d92b
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2016-06-23 16:28:46 -0400

    form values are getting cleared, but maybe state is not updating

---
+++ helpers/scrape5.js
@@ -0,0 +1,70 @@
+var webdriver = require('selenium-webdriver')
+var By = require('selenium-webdriver').By
+var until = require('selenium-webdriver').until
+var driver = new webdriver.Builder().forBrowser('firefox').build();
+
+// NAVIGATE TO HOME PAGE
+
+driver.get('http://localhost:3000/');
+printURL()
+
+// NAVIGATE TO EDIT PAGE
+
+driver.findElement(By.className("btn-edit-robot")).click();
+printURL()
+
+
+// CLEAR FORM VALUES
+
+printFormValues()
+clearFormValues()
+printFormValues()
+
+// SUBMIT FORM
+driver.findElement(By.xpath('//button[@type="submit"]')).click();
+printURL()
+printMessages()
+
+//
+// HELPER FUNCTIONS
+//
+
+function printURL(){
+  driver.getCurrentUrl().then(function(url){
+    console.log(url);
+  })
+}
+
+function printFormValues(){
+  driver.findElement(By.name('robotName')).getAttribute("value").then(function(inputVal){
+    console.log("robotName -- ", inputVal)
+  })
+
+  driver.findElement(By.name('robotDescription')).getAttribute("value").then(function(inputVal){
+    console.log("robotDesc -- ", inputVal)
+  })
+}
+
+function clearFormValues(){
+  //driver.findElement(By.name('robotName')).sendKeys("")
+  driver.findElement(By.name("robotName")).then(function(element){
+    element.clear()
+  })
+
+  //driver.findElement(By.name('robotDescription')).sendKeys("");
+  driver.findElement(By.name("robotDescription")).then(function(element){
+    element.clear()
+  })
+}
+
+function printMessages(){
+  driver.findElements(By.css("div .alert"))
+    .then(function(messages){
+      messages.forEach(function(message){
+        message.getText()
+          .then(function(messageText){
+            console.log("MESSAGE", messageText)
+          })
+      })
+  })
+}
